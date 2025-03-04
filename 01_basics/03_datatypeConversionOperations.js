78e133571b4c587e091397212f79de2412bca84c
Vineet Arora <37164735+VineetArora10@users.noreply.github.com>
2025-03-04 07:52:40 +0000

    Conversion operations in javascript

--- 01_basics/03_datatypeConversionOperations.js
+++ 01_basics/03_datatypeConversionOperations.js
@@ -30,3 +30,47 @@ console.log(booleanIsLoggedIn); // true
 let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber, typeof stringNumber); 
+
+// ********** Operations **********
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
+
+console.log(2 + 5); // 7
+console.log(2 - 5); // -3
+console.log(2 * 5); // 10
+console.log(2 ** 5); // 32
+console.log(2 / 5); // 0.4
+console.log(2 % 5); // 2
+
+let str1 = "Mercedes";
+let str2 = "Benz";
+let str3 = str1 + " " + str2;
+console.log(str3); // Mercedes Benz
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 5); // 125
+console.log(1 + 2 + "5"); // 35
+
+console.log(3 + 4 * 5 % 3); // 5
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(-true); // -1
+console.log(-false); // -0
+
+console.log(+""); // 0
+console.log(+"test"); // NaN
+
+let num1, num2, num3;
+num1 = num2 = num3 = 100;
+
+let gameCounter = 100;
+console.log(gameCounter); // 100
+gameCounter++; // postfix increment operator => value is incremented after the usage
+console.log(gameCounter); // 101
+++gameCounter; // prefix increment operator => value is incremented before the usage
+console.log(gameCounter); // 102
