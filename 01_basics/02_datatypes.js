82b186f177401fad14d3b7890d05dc1888c0d6f4
Vineet Arora <37164735+VineetArora10@users.noreply.github.com>
2025-03-04 07:14:29 +0000

    Datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as per newer version of js standard.
+
+// alert("Good Morning :)"); // Error is occurred as we are using node.js instead of browser.
+
+let name = "Vineet";
+let age = 25;
+let isLoggedIn = true;
+let city;
+
+// number => 2**53
+// string => '' or ""
+// bigint
+// boolean => true or false
+// null => it is a standalone value, it is a representation of an empty value
+// undefined => we have just declared a variable and not assigned any value, so it holds undefined in memory
+// symbol => unique
+// object
+
+console.log(typeof "london"); // string
+console.log(typeof 100); // number 
+console.log(typeof null); // object => it is an error in js
+console.log(typeof undefined); // undefined
